11646741db742caad190870b43511197711ed9b2
CHUANG MENG HSUAN <mxuannc@gmail.com>
2026-08-02 21:14:40 +0800

    fix: correct base path to apple-pie-project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/littlebeast_studio/",
+  base: "/apple-pie-project/",
   plugins: [vue()]
 })

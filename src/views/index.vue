0b7485458d415e4c62dd23dbe89877ee0e66302b
CHUANG MENG HSUAN <mxuannc@gmail.com>
2026-09-18 05:44:49 +0800

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -316,7 +316,7 @@ onUnmounted(() => {
                   </div>
                   <div>
                       <span class="font-bold">填寫報名表單：</span>
-                      <p>建議預留 10 分鐘填寫，表單將於 8/14（五）截止！</p>
+                      <p>建議預留 10 分鐘填寫，報名採額滿為止！</p>
                   </div>
                 </li>
                 <li>
